9668ac9b0a68b78880901dacbc6dc3676083f95a
shah- <artashes.shahinyan@gmail.com>
2014-06-02 20:03:13 +0400

    Removed rate.sh

--- rate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#gats currency rates from ACBA first page
-while true
-do
-wget -q -O- acba.am | egrep -o '<td>[0-9.]+</td>' | egrep -o '[0-9.]+' | { read -d \n -a arr;echo "ACBA $(date) $(date +%s) ${arr[@]}"; } >> ~/data/rate.log
-sleep 43200
-done
-
