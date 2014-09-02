4147604d9e9e3252d8a57c8a138a9b0226770ff3
shah- <artashes.shahinyan@gmail.com>
2014-09-03 00:11:07 +0400

    Added comments

--- calc.sh
+++ calc.sh
@@ -9,21 +9,30 @@ make_table() {
   local vp_month
   declare -a dt
   local idx
+#ֆայլի անունը ֊ ուսանողի անունն է
   while read name
   do
     echo "$name ․․․" >&2
     cat "$name" | {
+#առաջին երկու տողերը ֊ մեկ դասի գումարը և մեկ ակադեմիական ամսում պարապմունքների քանակն է
       read price && read vp_month &&
       while read -a line
       do
+#տողը պետք է պարունակի 2 կամ 3 դաշտ
         ((${#line[@]} != 2 && ${#line[@]} != 3 )) && continue
         dt=(${line[1]//./" "})
+#դաս՝ + օր․ամիս․տարի
+# + 29.01.14
         ((${#line[@]} == 2 )) && [[ ${line[0]} == + ]] &&
           printf "դաս\t%s-%s\t%s\t\"%s\"\t%s\t%s\t%d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month 0 &&
           continue
+#մուծում՝ գումար օր․ամիս․տարի
+# 5000 29.01.14
         ((${#line[@]} == 2 )) && [[ ${line[0]} != + ]] &&
           printf "մուտք\t%s-%s\t%s\t\"%s\"\t%s\t%s\t%d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month ${line[0]} &&
           continue
+#դաս և մուծում՝ + օր․ամիս․տարի գումար
+# + 29.01.14 5000
         ((${#line[@]} == 3)) &&
           printf "դաս\t%s-%s\t%s\t\"%s\"\t%s\t%s\t%d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month 0 &&
           printf "մուտք\t%s-%s\t%s\t\"%s\"\t%s\t%s\t%d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month ${line[2]} &&
