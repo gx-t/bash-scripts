c8299b274b3d0a69482ae92508a4a71f712673a9
shah- <artashes.shahinyan@gmail.com>
2014-09-05 00:36:29 +0400

    Added process

--- calc.sh
+++ calc.sh
@@ -15,6 +15,7 @@ make_table() {
   do
     echo "$name ․․․" >&2
     cat "$name" | {
+      name=${name//\ /_}
 #առաջին երկու տողերը ֊ մեկ դասի գումարը և մեկ ակադեմիական ամսում պարապմունքների քանակն է
       read price && read vp_month &&
       while read -a line
@@ -25,17 +26,17 @@ make_table() {
 #դաս՝ + օր․ամիս․տարի
 # + 29.01.14
         ((${#line[@]} == 2 )) && [[ ${line[0]} == + ]] &&
-          printf "%s-%s %s \"%s\" %s %s %d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month 0 &&
+          printf "%s-%s %s %s %s %s %d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month 0 &&
           continue
 #մուծում՝ գումար օր․ամիս․տարի
 # 5000 29.01.14
         ((${#line[@]} == 2 )) && [[ ${line[0]} != + ]] &&
-          printf "%s-%s %s \"%s\" %s %s %d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month ${line[0]} &&
+          printf "%s-%s %s %s %s %s %d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month ${line[0]} &&
           continue
 #դաս և մուծում՝ + օր․ամիս․տարի գումար
 # + 29.01.14 5000
         ((${#line[@]} == 3)) &&
-          printf "%s-%s %s \"%s\" %s %s %d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month  ${line[2]}
+          printf "%s-%s %s %s %s %s %d\n" ${dt[2]} ${dt[1]} ${dt[0]} "$name" $price $vp_month  ${line[2]}
           continue
       done
     }
@@ -43,18 +44,22 @@ make_table() {
 }
 
 process() {
-  calc() {
-    echo "$3"
-  }
   declare -A income_per_month
-  local line
-  while read line
+  declare -a line
+  while read -a line
   do
-    calc $line 
+    ((income_per_month[${line[0]}] += ${line[5]}))
+#    echo ${line[2]}
+  done
+  local idx_sorted=${!income_per_month[@]}
+  idx_sorted=$(sort <<< "${idx_sorted// /$'\n'}")
+  for idx in $idx_sorted
+  do
+    echo "Մուտքերը ըստ ամիսների՝ $idx ... ${income_per_month[$idx]}"
   done
 #    ((income_per_month[${line[0]}] += ${line[5]}))
 }
 
 
-ls | make_table
+ls | make_table | process
 
