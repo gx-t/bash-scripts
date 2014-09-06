4f7d4e57203d4e871b605b33e2d85f3889a4f4fb
shah- <artashes.shahinyan@gmail.com>
2014-09-06 20:36:28 +0400

    Finished version 0.1

--- calc.sh
+++ calc.sh
@@ -3,15 +3,18 @@
 ! cd Դասեր 2> /dev/null && echo "Չի ստացվում տեղափոխվել «Դասեր»։ Ստուգեք ճանապարհը։" >&2 && exit 1
 
 make_table() {
+  local debt
+  local income=0
+  local lesson_count=0
+  local last_month
   local name
   declare -a line
   local price
   local vp_month
   declare -a dt
-  local idx
   while read name
   do
-#    echo "$name ․․․" >&2
+    echo "$name ․․․" >&2
     cat "$name" | {
       name=${name//\ /_}
       read price && read vp_month &&
@@ -19,55 +22,76 @@ make_table() {
       do
         ((${#line[@]} != 2 && ${#line[@]} != 3 )) && continue
         dt=(${line[1]//./" "})
-        ((${#line[@]} == 2 )) && [[ ${line[0]} == + ]] &&
-          printf "${dt[2]}-${dt[1]} $name $price $vp_month 0\n" &&
+        local month="${dt[2]}/${dt[1]}"
+        last_month=${last_month:-$month}
+        [[ $last_month != $month ]] &&
+        {
+          ((debt=$income/$price-$lesson_count))
+          echo "$last_month $name $income $lesson_count $debt $price"
+          income=0
+          lesson_count=0
+          last_month=$month
+        }
+        ((${#line[@]} == 2 )) && [[ ${line[0]} == + ]] && ((lesson_count++)) &&
           continue
-        ((${#line[@]} == 2 )) && [[ ${line[0]} != + ]] &&
-          printf "${dt[2]}-${dt[1]} $name $price $vp_month ${line[0]}\n" &&
+        ((${#line[@]} == 2 )) && [[ ${line[0]} != + ]] && ((income+=${line[0]})) &&
           continue
-        ((${#line[@]} == 3)) &&
-          printf "${dt[2]}-${dt[1]} $name $price $vp_month  ${line[2]}\n" &&
+        ((${#line[@]} == 3)) && ((lesson_count++)) && ((income+=${line[2]})) &&
           continue
       done
+      ((debt=$income/$price-$lesson_count))
+      echo "$last_month $name $income $lesson_count $debt $price"
     }
   done
 }
 
 process() {
-  local stpm_payment
-  local pm_payment
+  local income=0
+  local debt=0
+  local debt_dr=0
+  local st_debt=0
   local last_month
-  local last_name
   declare -a line
   while read -a line
   do
-    local month=${line[0]}
-    local name=${line[1]}
-    local price=${line[2]}
-    local vp_month=${line[3]}
-    local payment=${line[4]}
+    last_month=${last_month:-${line[0]}}
+    [[ $last_month != ${line[0]} ]] &&
+    {
+      echo "Տարին/ամիսը........... $last_month"
+      echo "Աշակերտի անունը....... Բոլորը միասին"
+      echo "Գումարային մուտքերը... $income dram"
+      (($debt < 0)) || echo "Իմ Պարտքը............. $debt դաս"
+      (($debt_dr < 0)) || echo "Աշակերտների պարտքը.... $debt_dr դրամ"
+      income=0
+      debt=0
+      last_month=${line[0]}
+      echo "======================"
+      echo
+    }
+    ((income += ${line[2]}))
+    ((debt += ${line[4]}))
+    ((debt_dr += -${line[4]} * ${line[5]}))
+    ((st_debt = -${line[4]} * ${line[5]}))
+    echo "Տարին/ամիսը........... $last_month"
+    echo "Աշակերտի անունը....... ${line[1]//_/\ }"
+    echo "Մուտքերը.............. ${line[2]} դրամ"
+    echo "Դասերի քանակը......... ${line[3]}"
+    ((${line[4]} < 0)) || echo "Իմ Պարտքը............. ${line[4]} դաս"
+    (($st_debt < 0)) || echo "Աշակերտի պարտքը....... $st_debt դրամ"
+    echo "Մեկ դասի արժեքը....... ${line[5]} դրամ"
+    echo "---------------"
+    echo
     
-    last_month=${last_month:-$month}
-    last_name=${last_name:-$name}
-    ((pm_payment += $payment))
-    ((stpm_payment += $payment))
-    [[ $last_month != $month ]] &&
-    {
-      echo "MONTH CHANGE ($last_month:$pm_payment, $last_name:$stpm_payment)"
-      pm_payment=0
-      stpm_payment=0
-    } && last_month=$month
-
-    [[ $last_name != $name ]] &&
-    {
-      echo "NAME CHANGE ($last_name:$stpm_payment)"
-      stpm_payment=0
-    } && last_name=$name
-
-    echo "m=$month, n=$name, p=$price, v=$vp_month, p=$payment"
+#    echo "m=${line[0]},n=${line[1]},i=${line[2]},c=${line[3]},d=${line[4]},p=${line[5]}"
   done
-  echo "END ($last_month:$pm_payment, $last_name:$stpm_payment)"
+  echo "Տարին/ամիսը........... $last_month"
+  echo "Աշակերտի անունը....... Բոլորը միասին"
+  echo "Գումարային մուտքերը... $income dram"
+  (($debt < 0)) || echo "Իմ Պարտքը............. $debt դաս"
+  (($debt_dr < 0)) || echo "Աշակերտների պարտքը.... $debt_dr դրամ"
+  echo "======================"
 }
 
-ls | make_table | sort | process
+ls | make_table | sort -r | process > ../Հաշվետվություն.txt
+gedit ../Հաշվետվություն.txt
 
