958193f740f8886e0d59ba06c942b09b84998151
shah- <artashes.shahinyan@gmail.com>
2014-12-22 00:42:15 +0400

    removed titles

--- sst-cgi.sh
+++ sst-cgi.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 check-user-password() {
-        local password
-        read uid && read password && cd 2> /dev/null "$uid" && [[ "$password" == `cat ".password"` ]] && echo "$uid" && return 0
-        echo "Authorization failed"
+	local password
+	read uid && read password && cd 2> /dev/null "$uid" && [[ "$password" == `cat ".password"` ]] && echo "$uid" && return 0
+	echo "Authorization failed"
 	return 1
 }
 
@@ -17,7 +17,7 @@ register() {
 	local uid
 	read password && (( ${#password} < 5 )) && echo "ERR" && echo "Password must be at least 5 characters long" && return 
 	uid=$(uuidgen)
-	mkdir "$uid" && cd "$uid" && mkdir "inbox" && echo "$password" > ".password" && cat > .info && echo "OK" && echo "$uid"
+	mkdir "$uid" && cd "$uid" && mkdir "inbox" && mkdir "contacts" && mkdir "reverse" && echo "$password" > ".password" && cat > .info && echo "OK" && echo "$uid"
 }
 
 login() {
@@ -34,7 +34,7 @@ login() {
 message() {
 	local uid
 	local dest
-	local msg;
+	local msg
 	uid=`check-user-password` || return
 	msg="msg-$(uuidgen)"
 	read dest && cd "$dest/inbox" 2> /dev/null && tmp=`tempfile` && echo "$uid" > "$tmp" && cat >> "$tmp" && mv "$tmp" "$msg" && echo "OK" && return
@@ -42,9 +42,32 @@ message() {
 	echo "Error registering message. Invalid user id?"
 }
 
+add-contact() {
+	local uid
+	local uid1
+	uid=`check-user-password` || return
+	read uid1 && ln -s "../../$uid1" "$uid/contacts/$uid1" && ln -s "../../$uid" "$uid1/reverse/$uid" && echo "OK" && return
+	echo "ERR"
+	echo "Error adding to contact list. Invalid user id?"
+}
+
+remove-contact() {
+	local uid
+	local uid1
+	uid=`check-user-password` || return
+	read uid1 && rm "$uid/contacts/$uid1" && rm "$uid1/reverse/$uid" && echo "OK" && return
+	echo "ERR"
+	echo "Error removing contact from list. Invalid user id?"
+}
+
 cd users || exit 1
 
 printf "\n\n"
 read cmd
-[[ "$cmd" == "echo-line" || "$cmd" == "register" || "$cmd" == "login" || "$cmd" == "message" ]] && "$cmd"
+[[ "$cmd" == "echo-line" ||
+	"$cmd" == "register" ||
+	"$cmd" == "login" ||
+	"$cmd" == "message" ||
+	"$cmd" == "add-contact" ||
+	"$cmd" == "remove-contact" ]] && "$cmd"
 
