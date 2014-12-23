663e8a0326173860314a86f5990c4cb4b5a59c6b
shah- <artashes.shahinyan@gmail.com>
2014-12-23 23:41:23 +0400

    Made titles more compact and undestandable

--- rate-cmd.sh
+++ rate-cmd.sh
@@ -14,14 +14,10 @@ set key reverse Left outside;
 set grid;
 set style data linespoints;
 set multiplot layout 4, 1;
-set ylabel 'Dram/USD';
-plot '$ff' using 2:4 title 'Sell', '' using 2:5 title 'Buy', '' using 2:6 title 'CB';
-set ylabel 'Dram/Euro';
-plot '$ff' using 2:7 title 'Sell', '' using 2:8 title 'Buy', '' using 2:9 title 'CB';
-set ylabel 'Dram/Lari';
-plot '$ff' using 2:10 title 'Sell', '' using 2:11 title 'Buy', '' using 2:12 title 'CB';
-set ylabel 'Dram/RUR';
-plot '$ff' using 2:13 title 'Sell', '' using 2:14 title 'Buy', '' using 2:15 title 'CB';
+plot '$ff' using 2:4 title 'USD->Dram', '' using 2:5 title 'Dram->USD', '' using 2:6 title 'CB';
+plot '$ff' using 2:7 title 'Euro->Dram', '' using 2:8 title 'Dram->Euro', '' using 2:9 title 'CB';
+plot '$ff' using 2:10 title 'Lari->Dram', '' using 2:11 title 'Dram->Lari', '' using 2:12 title 'CB';
+plot '$ff' using 2:13 title 'RUR->Dram', '' using 2:14 title 'Dram->RUR', '' using 2:15 title 'CB';
 unset multiplot;
 " | curl -s -X POST shah32768.sdf.org/cgi-bin/rate.cgi?upload-usd-png --upload-file "-" 
 rm "$ff" 
