9e6277f8539e117f0519265dbbe570aed3529bfa
shah- <artashes.shahinyan@gmail.com>
2015-10-09 01:22:43 +0400

    Added youtube-download.sh - zenity and youtube-dl based

---
+++ youtube-download.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+url=$(xclip -o)
+url=$(zenity --width=320 --title="Բեռնել youyube -ից" --ok-label="Սկսել" --entry --text="Հասցեն․" --entry-text="$url")
+youtube-dl -c -l $url | while read line
+do
+	echo "# $line"
+done |
+zenity --progress   --title="Folder music review..."   --text=""   --pulsate --auto-kill
+
